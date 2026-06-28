91b0560bceeef61f9ca15ab16d21fe1384ea7883
Sopulu920 <michaelmorah91@gmail.com>
2026-06-28 22:55:07 +0100

    [ADD] info cards

--- Widget/Home/index.tsx
+++ Widget/Home/index.tsx
@@ -1,9 +1,26 @@
+"use client";
 import style from "./style.module.css";
 import Image from "next/image";
 import stem from "../../public/stem.svg";
 import file from "../../public/file.svg";
 
 export const HomePage = () => {
+
+    const infoTexts = [
+        {
+            bigText: "6+ Topics",
+            smallText: "ON JSS 1 MATHS"
+        },
+        {
+            bigText: "100%",
+            smallText: "OFFLINE READY"
+        },
+        {
+            bigText: "FREE",
+            smallText: "FOR STUDENTS"
+        }
+    ];
+
     return (
         <div className={style.container}>
 
@@ -17,8 +34,9 @@ export const HomePage = () => {
                         and enjoyable way!
                     </div>
 
-                    <div>
-                        other content
+                    <div className={style.btnContainer}>
+                        <button className={style.getStartedBtn}>Get Started</button>
+                        <button className={style.alreadyBtn}>I already have an account</button>
                     </div>
 
                 </div>
@@ -35,6 +53,21 @@ export const HomePage = () => {
 
             </div>
 
-        </div>
+            <div className={style.infoRow}>
+
+                {infoTexts.map((infoText, index) => (
+                    <div className={style.infoCard}>
+
+                        <div className={style.infoCardBigText}>{infoText.bigText}</div>
+                        <div className={style.infoCardSmallText}>{infoText.smallText}</div>
+
+                    </div>
+                ))}
+
+
+            </div>
+
+
+        </div >
     );
 }
