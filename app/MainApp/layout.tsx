fd625dfbde0a8339eeed41404049c854111d6cfc
HEnry Anowi <anowihenry@gmail.com>
2026-07-11 18:36:01 +0100

    Navbar readded

--- app/MainApp/layout.tsx
+++ app/MainApp/layout.tsx
@@ -1,6 +1,9 @@
+import {NavBar} from "@/components/ui/NavBar";
+
 const MainAppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
+      <NavBar />
       {children}
     </main>
   );
